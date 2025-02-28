452dd68a4d1ca001926e6826bfca175db3c7dceb
alihaiderbrandjaws <alihaider.brandjaws@gmail.com>
2025-02-28 22:33:16 +0500

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,8 @@
+var age = 18;
+var b = age >= 18 ? "true" : "false"; // ternary operators //
+document.write(b);
+
+let age = 20;
+let status = (age >= 18) ? "Adult" : "Minor";
+
+console.log(status); // Output: "Adult"
