f262824f1ca86fe254ea0113d032ac925ad20e6d
alihaiderbrandjaws <alihaider.brandjaws@gmail.com>
2025-02-28 20:47:12 +0500

    add caption

--- var.js
+++ var.js
@@ -1,4 +1,11 @@
-// how to write var variable //
+// In JavaScript, the var keyword is used to declare variables. However, it has some unique characteristics compared to let and const: //
+
+// Function Scope – A var variable is only scoped to the function in which it is declared. If declared outside any function, it becomes globally accessible. //
+
+// Hoisting – Variables declared with var are hoisted to the top of their scope but are initialized as undefined until the code execution reaches the assignment. //
+
+// Re-declaration Allowed – You can re-declare the same variable within the same scope without errors. //
+
 
 var Practice = "hello world <br>";
 document.write(Practice);
